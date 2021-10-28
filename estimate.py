21a8e0b83fc7a3403480193910a5250db6b24c5c
Ryan <ryoung@archer.feldmach.brandeis.edu>
2021-10-28 18:08:02 -0400

    Exposed interface to call from command line. (Using this to call from matlab).

--- estimate.py
+++ estimate.py
@@ -12,36 +12,44 @@
 # "savefile": a set of estimated PSPs.
 # Replace this with the file name of estimated PSPs.
 #----------------------------------
+#
+# Modified by Ryan Y to have command line interface -- helps expose an
+# interface to external callers, e.g. matlab
 
 import sys
 import os
 import time
 import pickle
 import numpy as np
-sys.path.append('modules/')
+sys.path.append('/home/ryoung/Code/analysis/+connectivity/+CoNNECT/modules/')
 from utils import load_data, pickup_bin, toPSPmatrix
 from makeCC import makeCC_allpair
 
-import tensorflow as tf
-#
-# from tensorflow.keras import backend
-# gpuConfig = tf.ConfigProto(allow_soft_placement=True,
-#                            gpu_options=tf.GPUOptions(allow_growth=True,
-#                                                      visible_device_list="1"))
-
-# sess = tf.Session(config=gpuConfig)
-# backend.set_session(sess)
-from tensorflow.keras.models import load_model
 
 
-def estimate(spikefile, savefile, min_s=2, conn_threshold=0.5, N_thred=1):
+def estimate(spikefile, savefile, min_s=2, conn_threshold=0.5, N_thread=1):
     """
     Estimate a connection matrix from a set of spike trains.
-    args:
-    spikefile: a set of spike trains. Spike trains are partitioned with semicolons ";". Each spike train consists of spike times partitioned by return codes.
-    min_s: an interval for which a center of cross-correlogram is deleted. One can exclude spike records at an interval of ± min_s　ms in the cross-correlogram because near-synchronous spikes were not detected in the experiment due to the shadowing effect.
-    conn_threshold: a threshold with which the presence or absence of connectivity is decided.
-    N_thred: The number of threads in the parallel calculation. If the number of neurons is about 50, the computation time might become even longer if you increase the number of threads.
+
+    ------
+    Input
+    ------
+
+    spikefile: a set of spike trains. Spike trains are partitioned with
+    semicolons ";". Each spike train consists of spike times partitioned by
+    return \n codes.
+
+    min_s: an interval for which a center of cross-correlogram is deleted. One
+    can exclude spike records at an interval of ± min_s　ms in the
+    cross-correlogram because near-synchronous spikes were not detected in the
+    experiment due to the shadowing effect.
+
+    conn_threshold: a threshold with which the presence or absence of
+    connectivity is decided.
+
+    N_thread: The number of threads in the parallel calculation. If the number
+    of neurons is about 50, the computation time might become even longer if
+    you increase the number of threads.
     """
     # loading the spike train data:
     spiketrain = load_data(spikefile)
@@ -50,7 +58,7 @@ def estimate(spikefile, savefile, min_s=2, conn_threshold=0.5, N_thred=1):
     # compute the cross correlogram (CC):
     Begin = -50 - min_s
     End = 50 + min_s
-    X, index = makeCC_allpair(spiketrain, Begin, End, N_thred)
+    X, index = makeCC_allpair(spiketrain, Begin, End, N_thread)
 
     # delete a central part of cross-correlogram:
     print("Delete min_s")
@@ -62,6 +70,14 @@ def estimate(spikefile, savefile, min_s=2, conn_threshold=0.5, N_thred=1):
     X = pickup_bin(X, begin0, end0, begin1, end1)
 
     # load a model:
+    import tensorflow as tf
+    from tensorflow.compat.v1.keras import backend
+    gpuConfig = tf.compat.v1.ConfigProto(allow_soft_placement=True,
+                               gpu_options=tf.compat.v1.GPUOptions(allow_growth=True,
+                                                         visible_device_list="0"))
+    sess = tf.compat.v1.Session(config=gpuConfig)
+    backend.set_session(sess)
+    from tensorflow.keras.models import load_model
     print("Load a CoNNECT model")
     model = load_model("model/model.h5")
 
@@ -82,10 +98,19 @@ def estimate(spikefile, savefile, min_s=2, conn_threshold=0.5, N_thred=1):
           delimiter=",")
 
 if __name__ == "__main__":
-    spikefile = "sample/spiketrain.txt"
-    savefile = "sample/estimated.csv"
 
-    estimate(spikefile, savefile)
+    from argparse import ArgumentParser
+    parser = ArgumentParser('Connect', 'Monosynaptic analysis')
+    parser.add_argument('--spikesfile', default="sample/spiketrain.txt")
+    parser.add_argument('--savefile', default="sample/estimated.csv")
+    parser.add_argument('--min_s', default=float(2.0), type=float, action="store")
+    parser.add_argument('--conn_threshold', default=float(0.5), type=float)
+    parser.add_argument('--N_thread', default=1, type=int)
+    args = parser.parse_args()
+
+
+    estimate(args.spikesfile, args.savefile, min_s=args.min_s,
+             conn_threshold=args.conn_threshold, N_thread=args.N_thread)
 
 ##########################################################################
 # Python program contributed by Daisuke Endo (daisuke.endo96@gmail.com).
